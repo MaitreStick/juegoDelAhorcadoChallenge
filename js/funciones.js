f558e3fa95860e7942b8c61c2a52f598fafd0c07
Maitre Stick <mastijaci@gmail.com>
2022-05-30 12:28:53 -0500

    subiendo archivos nuevos, segunda parte

--- js/funciones.js
+++ js/funciones.js
@@ -1,6 +1,18 @@
 var btnIniciar = document.querySelector("#iniciarJuego");
+var btnAgregarPalabra = document.querySelector("#agregarPalabra");
+var btnCancelar = document.querySelector("#cancelar");
 
 btnIniciar.addEventListener("click", function(event){
     event.preventDefault();
-    location.href = "juego.html";
+    location.href = "palabra.html";
+});
+
+btnAgregarPalabra.addEventListener("click", function(event){
+    event.preventDefault();
+    location.href = "palabra.html";
+});
+
+btnCancelar.addEventListener("click", function(event){
+    event.preventDefault();
+    location.href = "index.html";
 });
